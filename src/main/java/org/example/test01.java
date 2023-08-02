757475f34b14148a56d57c6404c46ff984b2be0c
zhi.ling <zhi.ling@transwarp.io>
2023-08-02 19:55:55 +0800

    new branch new commit

--- src/main/java/org/example/test01.java
+++ src/main/java/org/example/test01.java
@@ -3,5 +3,11 @@ package org.example;
 public class test01 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
